6c29b98ba5c8f86204352f9021114fd0bc33d251
Riswangans <101172116+Riswangans@users.noreply.github.com>
2025-11-22 20:30:56 +0700

    Implement auto slide for testimonial section
    Added auto slide functionality for testimonials and improved button interactions.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,54 @@
-const btnLihat = document.getElementById('btn-lihat');
-    const btnTutup = document.getElementById('btn-tutup');
-    const testimoniLengkap = document.getElementById('testimoni-lengkap');
+// --- Auto Slide Testimonial ---
+const slider = document.getElementById("testimonialSlider");
+let autoScroll = true;
 
-    btnLihat.addEventListener('click', () => {
-        testimoniLengkap.style.display = 'flex';
-        btnLihat.style.display = 'none';
-        btnTutup.style.display = 'inline-block';
-    });
+function autoSlide() {
+  if (!autoScroll) return;
 
-    btnTutup.addEventListener('click', () => {
-        testimoniLengkap.style.display = 'none';
-        btnLihat.style.display = 'inline-block';
-        btnTutup.style.display = 'none';
-        window.scrollTo({ top: document.getElementById('testimoni-awal').offsetTop - 120, behavior: 'smooth' });
-    });
+  slider.scrollBy({
+    left: slider.clientWidth,
+    behavior: "smooth"
+  });
+
+  // balik ke awal kalau sudah mentok
+  if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 10) {
+    setTimeout(() => {
+      slider.scrollTo({ left: 0, behavior: "smooth" });
+    }, 2000);
+  }
+}
+
+setInterval(autoSlide, 3000);
+
+// Stop auto slide jika user swipe
+slider.addEventListener("touchstart", () => {
+  autoScroll = false;
+});
+
+slider.addEventListener("touchend", () => {
+  setTimeout(() => autoScroll = true, 3000);
+});
+
+
+
+document.querySelectorAll(".btn-wa").forEach(btn => {
+  btn.addEventListener("click", function () {
+
+    const row = this.closest("tr");
+
+    const nama = row.querySelector(".nama")?.innerText || "-";
+    const desc = row.querySelector(".desc")?.innerText || "Tidak ada deskripsi";
+    const harga = row.querySelector(".harga")?.innerText || "-";
+
+    let pesan = `Halo, saya ingin membeli:\n\n`;
+    pesan += `Produk: *${nama}*\n`;
+    if (desc !== "-") pesan += `Deskripsi: ${desc}\n`;
+    pesan += `Harga: ${harga}\n\n`;
+    pesan += `Mohon diproses ya.`;
+
+    const encoded = encodeURIComponent(pesan);
+    const nomor = "18254875043";
+
+    window.open(`https://wa.me/${nomor}?text=${encoded}`, "_blank");
+  });
+});
